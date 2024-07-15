51358ae3ae383b824aa491cb29fd0d3456a7c37f
shridhar-biradar <shridharbiradar6361@gmail.com>
2024-07-15 23:19:41 +0530

    transpose of matrix

---
+++ Rotation.java
@@ -0,0 +1,24 @@
+
+public class Rotation {
+    
+        public static void main(String[] args) {
+        
+
+            int arr[][]={
+                {1,2,3},
+                {3,4,5},
+                {6,7,8}
+            };
+            
+
+              System.out.println(" transpoce matrix  values");
+              for(int i=0; i<3;i++){
+                for(int j=0;j<=2;j++){
+                    System.out.print(arr[j][i] + " ");
+                }
+                System.out.println("\n");
+              }
+
+        }             
+    
+}
