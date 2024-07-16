12b897dc84c908d3866ffcecf681fbc9ef470eb1
shridhar-biradar <shridharbiradar6361@gmail.com>
2024-07-16 16:30:44 +0530

    pallindrome

--- pallindrome.java
+++ pallindrome.java
@@ -1,11 +1,11 @@
 
 public class pallindrome {
 public static void main(String[] args) {
-    String s1="madam";
+    String s1="hello";
     String s2="";
     int n=s1.length();
     for(int i=0; i<n;i++){
-        s2=s1.charAt(i)+s2;
+        s2= s1.charAt(i)+s2;
     }
     if(s2.equals(s1)){
         System.out.println("it is pallindrome");
