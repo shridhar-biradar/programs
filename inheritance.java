242dab87646c2cd8026207050324a5cd16452a26
shridhar-biradar <shridharbiradar6361@gmail.com>
2024-07-16 18:57:26 +0530

    inheritance

---
+++ inheritance.java
@@ -0,0 +1,18 @@
+class human{
+    int age;
+    void sleep(){
+        age=22;
+        System.out.println("human nead good sleep");
+        System.out.println(age);
+    }
+}
+class student extends human{
+
+}
+
+public class inheritance {
+    public static void main(String[] args) {
+        student s1=new student();
+        s1.sleep();
+    }
+}
