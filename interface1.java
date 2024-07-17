c0acd320513f67340ee9a49abbef8f14e4f499cd
shridhar-biradar <shridharbiradar6361@gmail.com>
2024-07-17 20:01:04 +0530

    interface

--- interface1.java
+++ interface1.java
@@ -1,18 +1,18 @@
-// interface  A{
-//      void show();
-// }
-// class B implements  A{
-//     public void show()
-//     {
-//         System.out.println(" Interface is implimented in class B");
-//     }
-// }
+interface  A{
+     void show();
+}
+class B implements  A{
+    public void show()
+    {
+        System.out.println(" Interface is implimented in class B");
+    }
+ }
 
 public class  interface1{
     public static void main(String[] args){
-        // B obj=new B();
-        // obj.show();
+        B obj=new B();
+        obj.show();
 
-        System.out.println("hello");
+       
     }
 }
