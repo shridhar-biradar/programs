1523a4ab7a4e127f6489a53f34829ec7457e644f
shridhar-biradar <shridharbiradar6361@gmail.com>
2024-07-19 22:38:53 +0530

    Reversing array

--- reverse1.java
+++ reverse1.java
@@ -4,7 +4,7 @@ public class reverse1 {
         int n=arr.length-1;
         
         for(int i=n-1;i>0;i-- ){
-            System.out.println(arr[i]+ "  ");
+            System.out.println(arr[i]+ " ");
         }
         
     }
