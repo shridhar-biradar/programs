541582564c3e52bfe820fd258888b6c18d03d235
shridhar-biradar <shridharbiradar6361@gmail.com>
2024-07-19 22:37:49 +0530

    Reversing array

--- reverse1.java
+++ reverse1.java
@@ -1,9 +1,9 @@
 public class reverse1 {
     public static void main(String[] args) {
         int arr[]={1,2,3,4,5};
-        int n=arr.length;
+        int n=arr.length-1;
         
-        for(int i=n-1;i>=0;i--){
+        for(int i=n-1;i>0;i--){
             System.out.println(arr[i]+ " ");
         }
         
