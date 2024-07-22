e4f8f3c4eed3539764ba8195f874f4281d63285c
shridhar-biradar <shridharbiradar6361@gmail.com>
2024-07-22 11:07:15 +0530

    Factorial of num

---
+++ factorial1.java
@@ -0,0 +1,14 @@
+public class factorial1 {
+    public static int facto(int n){
+        if(n==1 || n==0){
+            return 1;
+        
+        }
+        else
+        return n*facto(n-1);
+    }
+    public static void main(String[] args) {
+        int n=4;
+        System.out.println(facto(n));
+    }
+}
