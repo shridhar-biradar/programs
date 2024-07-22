a5f3d83eccbb2bde4cf519b331ad31d033e4b88e
shridhar-biradar <shridharbiradar6361@gmail.com>
2024-07-22 11:17:23 +0530

    swaping two num

---
+++ swapping.java
@@ -0,0 +1,11 @@
+public class swapping {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        b=a+b;
+        a=b-a;
+        b=b-a;
+        System.out.println("a:" + a);
+        System.out.println("b:"+ b);
+    }
+}
