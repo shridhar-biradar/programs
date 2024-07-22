9ae74f571923cfe3bfb94257fd5470325fbf1e5d
shridhar-biradar <shridharbiradar6361@gmail.com>
2024-07-22 11:24:04 +0530

    primeNum

---
+++ primeNum.java
@@ -0,0 +1,21 @@
+public class primeNum {
+    public static boolean isPrime(int n){
+        if(n==0 || n==1){
+            return false;
+        }
+        if(n==2){
+            return  true;
+        }
+        for(int i=2;i<n;i++){
+            if(n%2==0){
+                return false;
+            }
+            
+        } return true;
+    }
+
+    public static void main(String[] args) {
+        int n=4;
+        System.out.println(isPrime(n));
+    }
+}
