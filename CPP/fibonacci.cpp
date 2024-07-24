7d9789617a614372dbea621d458ca76b40ae2fd6
shridhar-biradar <shridharbiradar6361@gmail.com>
2024-07-24 19:51:31 +0530

    fibonacci series

--- CPP/fibonacci.cpp
+++ CPP/fibonacci.cpp
@@ -1,9 +1,11 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int a=0,b=1,n=7,c;
+    int a=0,b=1,c;
+    int n=7;
     cout<<a <<" "<<b<<" ";
-    for(int i=2;i<=n;i++){
+    for(int i=2;i<=n;i++)
+    {
         c=a+b;
         cout<<" "<<c;
         a=b;
