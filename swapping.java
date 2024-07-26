bbedab6c48b355ab2b6d661ffb78103c4ba2aabf
shridhar-biradar <shridharbiradar6361@gmail.com>
2024-07-26 22:24:55 +0530

    swapping

--- swapping.java
+++ swapping.java
@@ -4,8 +4,8 @@ public class swapping {
         int b=20;
         b=a+b;
         a=b-a;
-        b=b-a;
-        System.out.println("a:   " +a);
+        b=b-a; 
+        System.out.println("a:    " +a);
         System.out.println("b:"+ b);
     }
 }
