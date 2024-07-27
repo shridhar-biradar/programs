f46afe55408165b68acde15c032c9e5f751e5c5e
shridhar-biradar <shridharbiradar6361@gmail.com>
2024-07-27 12:29:16 +0530

    bubble sorting

--- bubbleSort.java
+++ bubbleSort.java
@@ -1,38 +1,31 @@
 
 import java.util.Scanner;
 
-public class bubbleSort {
-
-    public static int arraySorting(int[] arr){
-        // int n;
+class bubbleSort{
+    public static void arraySorting(int[] arr){
         for(int i=0;i<arr.length;i++){
             for(int j=i+1;j<arr.length;j++){
                 if(arr[i]>arr[j]){
-                    int temp=arr[i];
+                    int temp= arr[i];
                     arr[i]=arr[j];
                     arr[j]=temp;
                 }
             }
         }
         for(int i=0;i<arr.length;i++){
-        System.out.print(arr[i]+" ");
-    }
-    return  0;
+            System.out.print(arr[i]+" ");
+        }
     }
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        System.out.println("enter length of arr:");
+    Scanner sc=new Scanner(System.in);
+        System.out.println("enter array size:");
         int n=sc.nextInt();
         int[] arr=new int[n];
-        System.out.println("enter element of arr:");
+        System.out.println("enter element of array:");
         for(int i=0;i<n;i++){
             arr[i]=sc.nextInt();
         }
-        // System.out.println(arraySorting(arr));
-        System.out.println("sorted array is :");
+        System.out.println(" elements of array are:");
         arraySorting(arr);
-        // for(int i=0;i<n;i++){
-        // System.out.println(arraySorting(arr));
-        // }
     }
 }
