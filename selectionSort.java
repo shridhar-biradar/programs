cda2843612271c922f3d7cbc04253f6fe6a28dcc
shridhar-biradar <shridharbiradar6361@gmail.com>
2024-07-30 22:32:14 +0530

    Selection sort

--- selectionSort.java
+++ selectionSort.java
@@ -4,31 +4,27 @@ import java.util.Arrays;
 
 public class selectionSort {
 
-    public static void SelectionSort(int arr[])
-    {
-       for(int i=0;i<arr.length;i++)
-       {
+   public static void SelectionSorting(int arr[]){
+    for(int i=0;i<arr.length;i++){
         int min_idx=i;
-        for(int j=i+1;j<arr.length;j++)
-        {
-            while(arr[j]<arr[min_idx])
-            {
+        for(int j=i+1;j<arr.length;j++){
+            if(arr[j]<arr[min_idx]){
                 min_idx=j;
             }
         }
-            if(min_idx!=i)
-            {
-                int temp=arr[min_idx];
-                arr[min_idx]=arr[i];
-                arr[i]=temp;
-            }
+        if(min_idx!=i ){
+            int temp=arr[min_idx];
+            arr[min_idx]=arr[i];
+            arr[i]=temp;
         }
-       }
-    
+    }
+}
+ 
+   
     
     public static void main(String[] args) {
-    int arr[]={1,5,2,78};
-    SelectionSort(arr);
+    int arr[]={20,10,15,40,30};
+    SelectionSorting(arr);
     System.out.println(Arrays.toString(arr));
     }
 }
