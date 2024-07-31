8d30e8bb30698c5f58d2097cafd2b0cac60f68b4
shridhar-biradar <shridharbiradar6361@gmail.com>
2024-07-31 21:54:09 +0530

    reverse string & removing large space

--- Reversing.java
+++ Reversing.java
@@ -1,20 +1,22 @@
+
+// import java.lang.reflect.AnnotatedArrayType;
+
 class  Reversing{
-    public static String stringReversing(String str){
+    public static String reversingString(String str){
         str=str.trim();
-        String words[]=str.split("\\s+");
+        String word[]=str.split("\\s+");
         StringBuilder ans=new StringBuilder();
-        for(int i=words.length-1;i>=0;i--){
-            ans.append(words[i]);
+        for(int i=word.length-1;i>=0;i--){
+            ans.append(word[i]);
             if(i!=0){
                 ans.append(" ");
             }
-
         }
         return ans.toString();
     }
     public static void main(String[] args) {
-        String str="          have   a   good day   ";
-        String result=stringReversing(str);
-        System.out.println(result);
+       String str="  have   a  nice   day   ";
+       String result=reversingString(str);
+       System.out.println(result);
     }
 }
