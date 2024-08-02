147911af9d028efe4c6e6c468db00aa9d5261903
shridhar-biradar <shridharbiradar6361@gmail.com>
2024-08-02 19:10:25 +0530

    pallindrome

--- pallindrome.java
+++ pallindrome.java
@@ -1,10 +1,9 @@
 
 public class pallindrome {
-    
+
 public static void main(String[] args) {
     String s1="hello";
     String s2="";
-
     int n=s1.length();
     for(int i=0; i<n;i++){
         s2= s1.charAt(i)+s2;
