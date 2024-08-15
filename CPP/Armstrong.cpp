9fbe3450bbfbbfac3f29f15ca4414842a2cc9c32
shridhar-biradar <shridharbiradar6361@gmail.com>
2024-08-15 20:44:26 +0530

    Armstrong number

--- CPP/Armstrong.cpp
+++ CPP/Armstrong.cpp
@@ -5,12 +5,13 @@ int main(){
 
    int num=153, temp, rem, reverse=0;
    temp=num;
+   
    while(temp!=0){
     rem=temp%10;
     reverse=reverse+(rem*rem*rem);
     temp=temp/10;
    }
-   
+
    if(num==reverse){
     cout<<" Armstrong num ";
    }
