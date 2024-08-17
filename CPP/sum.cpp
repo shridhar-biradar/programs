08e5971763bdd6399f732281287f3336911c444c
shridhar-biradar <shridharbiradar6361@gmail.com>
2024-08-17 22:00:50 +0530

    sum of number

--- CPP/sum.cpp
+++ CPP/sum.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 
 int sum(int num){
-    
+
     int result=0;
     for(int i=0;i<=num;i++){
         result+=i;
     }return result;
 }
 
-int main(){  
+int main(){   
     int num=7;
     cout<< sum(num);
     return 0;
