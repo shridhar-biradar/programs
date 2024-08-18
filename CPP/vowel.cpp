380b69d4c7692c864864ac01cd67e17ff8c354d8
shridhar-biradar <shridharbiradar6361@gmail.com>
2024-08-18 20:12:56 +0530

    finds character is vowel pr consonant

--- CPP/vowel.cpp
+++ CPP/vowel.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main(){ 
-    char ch='a';
+    char ch='c';
     if(isalpha(ch)){
         if(ch=='a' || ch=='e' || ch=='i' || ch== 'o' || ch=='u' || ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U'){
             cout<< ch<< " is vowel";
