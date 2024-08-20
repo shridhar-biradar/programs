b87ccfa0a468cf93f741537ba4fcfbc245605ecb
shridhar-biradar <shridharbiradar6361@gmail.com>
2024-08-20 22:46:15 +0530

    rotation of 2D matrix

--- Rotation.java
+++ Rotation.java
@@ -8,7 +8,7 @@ public class Rotation {
                 {1,2,3},
                 {3,4,5},
                 {6,7,8}
-            };
+            }; 
             
 <<<<<<< HEAD
               int i; 
