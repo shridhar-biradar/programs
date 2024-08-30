13352f930e29d212fdef9b28537b6a9c307c38fd
shridhar-biradar <shridharbiradar6361@gmail.com>
2024-08-30 22:29:17 +0530

    operators

---
+++ Python/29-08-24/dataType.py
@@ -0,0 +1,30 @@
+print('list example')
+a=[10,20,10,'rahul',2.3,False]
+print(a)
+print(type(a))
+print(len(a))
+print(a[0])
+
+print('------')
+
+print('set example')
+a={10,10,20,2.3,'rahul'}
+print(a)
+print(type(a))
+print(len(a))
+ 
+print('------')
+
+print('tuple example')
+a=(10,10,20,2.3,'rahul',True)
+print(a)
+print(type(a))
+print(len(a))
+print('----')
+
+print('dictionar example')
+a={ 1:10,2:20,3:30}
+print(a)
+print(type(a))
+print(len(a))
+print('-----')
---
+++ Python/29-08-24/demo.py
@@ -0,0 +1,15 @@
+age=23
+name='shridhar'
+x=12.5545451121141
+print(age)
+print(type(age))
+print(name)
+print(type(name))
+print('hello world')
+print(x+10)
+print(type(x))
+
+y=input('enter your name:')
+print(y)
+x=input("enter ur age:")
+print(x)
