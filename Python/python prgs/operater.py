c52fd0a868d955e1050959de0fdcdd4f783326b1
shridhar-biradar <shridharbiradar6361@gmail.com>
2024-08-30 23:00:25 +0530

    operators

--- Python/python prgs/operater.py
+++ Python/python prgs/operater.py
@@ -42,3 +42,10 @@ b=int(input('enter 2nd num:'))
 min=a if a<b else b
 print('minimum value is:' ,min)
 print()
+
+print('to find maximum of 3 numbers')
+a=int(input('enter 1st num:'))
+b=int(input('enter 2nd num:'))
+c=int(input('enter 3rd num:'))
+max=a if a>b and a>c else b if b>c else c
+print('maximum value:', max)
