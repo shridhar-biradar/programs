5a0bbce8315ab9ec32d440012deccbc2e8112a34
shridhar-biradar <shridharbiradar6361@gmail.com>
2024-09-01 19:42:00 +0530

    special operator

---
+++ Python/py prgs/id_operator.py
@@ -0,0 +1,11 @@
+print('identity operator')
+a=10
+b=20
+print('a=10 & b=20  a is b',a is b)
+print(id(a))
+print(id(b))
+a='shridhar'
+b='shri'
+print(id(a))
+print(id(b))
+print('a=shridhar b=shri  a is not b',a is not b)
