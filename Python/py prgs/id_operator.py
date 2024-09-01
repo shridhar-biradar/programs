282c95bc1f045c237d27aa30b7bc3561fab46206
shridhar-biradar <shridharbiradar6361@gmail.com>
2024-09-01 19:53:39 +0530

    special operators

--- Python/py prgs/id_operator.py
+++ Python/py prgs/id_operator.py
@@ -4,8 +4,17 @@ b=20
 print('a=10 & b=20  a is b',a is b)
 print(id(a))
 print(id(b))
+
 a='shridhar'
 b='shri'
 print(id(a))
 print(id(b))
 print('a=shridhar b=shri  a is not b',a is not b)
+
+list1=['one','two','three']
+list2=['one','two','three']
+print(id(list1))
+print(id(list2))
+print(list1 is list2)
+print(list1 is not list2)
+print(list1==list2)
