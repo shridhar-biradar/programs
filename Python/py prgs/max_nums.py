e78cbc0907aa3da34d90fa966244a019654a8750
shridhar-biradar <shridharbiradar6361@gmail.com>
2024-09-01 19:10:43 +0530

    maximum of 3 numbers

--- Python/py prgs/max_nums.py
+++ Python/py prgs/max_nums.py
@@ -1,4 +1,4 @@
-a=int(input('enter 1at number:'))
+a=int(input('enter 1at number: '))
 b=int(input('enter 2nd number: '))
 c=int(input('enter 3rd number: '))
 max_nums=a if a>b and a>c else b if b>c else c
