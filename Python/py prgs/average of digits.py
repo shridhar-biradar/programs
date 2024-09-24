17de7c30ff10794760c8e06a4a892cf47b7ca25c
shridhar-biradar <shridharbiradar6361@gmail.com>
2024-09-24 22:27:27 +0530

    average of digits in given number

---
+++ Python/py prgs/average of digits.py
@@ -0,0 +1,11 @@
+def avg(num):
+    sum=0
+    count=0
+    while num>0:
+        digit=num%10
+        sum=sum+digit
+        count=count+1
+        num=num//10
+    return sum/count
+num=int(input('enter the number: '))
+print(avg(num))
