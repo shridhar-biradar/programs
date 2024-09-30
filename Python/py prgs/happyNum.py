92c59c4af84292ca1721e91371390527c80551eb
shridhar-biradar <shridharbiradar6361@gmail.com>
2024-09-30 18:41:20 +0530

    Happy number or not

---
+++ Python/py prgs/happyNum.py
@@ -0,0 +1,14 @@
+def isHappy(num):
+    while num>9:
+        sum=0
+        while num!=0:
+            digit=num%10
+            sum=sum+digit**2
+            num=num//10
+        num=sum
+    return num==1 or num==7
+num=int(input('enter the number: '))
+if isHappy(num):
+    print(num ,' is happy number')
+else:
+    print(num, 'is not happay number')
