e456e19d03061b6ba9508def11dd76b6c94de635
shridhar-biradar <shridharbiradar6361@gmail.com>
2024-09-30 18:10:40 +0530

    ArmStrong number

--- Python/py prgs/armStrong.py
+++ Python/py prgs/armStrong.py
@@ -1,4 +1,5 @@
 # it is for only 3 digit number
+'''
 def isArmStrong(num):
     sum=0
     temp=num
@@ -12,3 +13,18 @@ if isArmStrong(no):
     print(no,' is a armStrong number')
 else:
    print(no,' is a not armStrong number')
+'''
+# To find armStrong number of all type input
+
+def isArmStrong(num):
+    sum=0
+    l=len(str(num))
+    for i in range(l):
+        sum=sum + int(num[i])**l
+    sum=str(sum)
+    return sum==num
+no=input('enter the number: ')
+if isArmStrong(no):
+    print(no, ' is armStrong Number')
+else:
+    print(no, 'is not armStrong number ')
