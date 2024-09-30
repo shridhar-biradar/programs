b4795aa2740ef3825f53ed607e9313974d84e040
shridhar-biradar <shridharbiradar6361@gmail.com>
2024-09-30 18:22:25 +0530

    program to find number is Emirp or not

---
+++ Python/py prgs/emirpNum.py
@@ -0,0 +1,22 @@
+def isPrime(num):
+    for i in range(2,num//2+1):
+        if num % i== 0:
+            return False
+        return True
+
+def getReverse(num):
+    rev=0
+    while num!=0:
+        digit=num%10
+        rev=rev*10 + digit
+        num=num//10
+    return rev
+
+def emirp(num):
+    return isPrime(num) and isPrime(getReverse(num))
+
+num=int(input('enter the number: '))
+if emirp(num):
+    print(num ,' is emirp number')
+else:
+    print(num ,'is not emirp number') 
