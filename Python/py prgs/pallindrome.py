5f0ea959cfdfbb0739802911b1f080d4e716c08b
shridhar-biradar <shridharbiradar6361@gmail.com>
2024-10-01 22:43:41 +0530

    number is pallindrome or not

---
+++ Python/py prgs/pallindrome.py
@@ -0,0 +1,14 @@
+def isPallindrome(num):
+    rev=0
+    temp=num
+    while num>0:
+        digit=num%10
+        rev=rev*10 + digit
+        num=num//10
+    return rev==temp
+
+num=int(input('enter number: '))
+if isPallindrome(num)==True:
+    print(num,' is a pallindrome number')
+else:
+    print(num,' is not a palindrome number')
