d6cc26b24f40a894d971912125cfd8709b1cf4d8
shridhar-biradar <shridharbiradar6361@gmail.com>
2024-10-01 22:27:53 +0530

    biggest digit in given number

---
+++ Python/py prgs/biggest digit.py
@@ -0,0 +1,12 @@
+def biggestNum(num):
+    max=0
+    while num>0:
+        digit=num%10
+        if digit>max:
+            max=digit
+        num=num//10
+    return max
+
+num=int(input('enter number to find out biggest digit: '))
+big_digit=biggestNum(num)
+print(bigg)
