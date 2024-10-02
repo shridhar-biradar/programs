43c2a2b91bb55d0f2feb3edaaf1f249f542526a0
shridhar-biradar <shridharbiradar6361@gmail.com>
2024-10-02 23:55:52 +0530

    membership operators

--- Python/py prgs/operators.py
+++ Python/py prgs/operators.py
@@ -3,7 +3,7 @@ x='hello learning python is very easy'
 print('h' in x)
 print('d' in x)
 print('d' not in x)
-print('python' in x)
+print('python' in x) 
 
 list=['sunny','bunny','chinny','pinny']
 print('sunny' in list)
